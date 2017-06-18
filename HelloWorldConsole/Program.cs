a9f883993a437750f8cd275defb527cac50ceac2
techiekats <kshah@xceligent.com>
2017-06-18 13:58:38 -0500

    code to read from api endpoint

--- HelloWorldConsole/Program.cs
+++ HelloWorldConsole/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Net.Http;
 
 namespace HelloWorldConsole
 {
@@ -10,6 +8,23 @@ namespace HelloWorldConsole
 	{
 		static void Main(string[] args)
 		{
+			string apiEndpoint = ConfigurationManager.AppSettings["helloworldservice-endpoint"];
+			GetHelloWorldText(apiEndpoint);
+			Console.WriteLine($"Waiting for response from {apiEndpoint}....");
+			Console.ReadKey();
+		}
+		private static async void GetHelloWorldText (string apiEndpoint)
+		{
+			HttpClient client = new HttpClient();
+			try
+			{
+				var responseText = await client.GetStringAsync(apiEndpoint);
+				Console.WriteLine(responseText);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
 		}
 	}
 }
